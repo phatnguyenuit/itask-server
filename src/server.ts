0e9ac18be4527e60303f459c1eb3f49f6ed7fd84
Nguyen Phat <phatnt.uit@gmail.com>
2021-12-02 22:42:16 +0700

    chore: configure context and landing page

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import { ApolloServer } from 'apollo-server';
 import { ApolloServerExpressConfig } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageDisabled } from 'apollo-server-core';
 
 import dataSources from './datasources';
 import schema from './schema';
 
+const plugins =
+  process.env.NODE_ENV === 'production'
+    ? [ApolloServerPluginLandingPageDisabled] // Disable landing page for production mode
+    : [];
+
 const apolloConfig: ApolloServerExpressConfig = {
   schema,
+  plugins,
   dataSources: () => dataSources,
+  nodeEnv: process.env.NODE_ENV,
+  context: ({ req }) => ({ headers: req.headers }),
 };
 
 // The ApolloServer constructor requires two parameters: your schema
