fbf46e5ab754e41834f778a4394a3dfccf37bfb0
Nguyen Phat <phatnt.uit@gmail.com>
2021-12-08 23:19:26 +0700

    fix: fix prepare script

--- prepare.js
+++ prepare.js
@@ -1,5 +1,6 @@
 const isCi = process.env.CI !== undefined;
+const isProduction = process.env.NODE_ENV === 'production';
 
-if (!isCi) {
+if (!isCi || !isProduction) {
   require('husky').install();
 }
